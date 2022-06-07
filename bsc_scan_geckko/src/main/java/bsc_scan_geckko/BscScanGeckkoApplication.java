de05fa738a7466b65abe733010d5884b468b2d2a
DuyDK <khanhduyapt@gmail.com>
2022-06-07 10:17:23 +0700

    update

--- bsc_scan_geckko/src/main/java/bsc_scan_geckko/BscScanGeckkoApplication.java
+++ bsc_scan_geckko/src/main/java/bsc_scan_geckko/BscScanGeckkoApplication.java
@@ -2,7 +2,6 @@ package bsc_scan_geckko;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Objects;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,59 +17,59 @@ import lombok.extern.slf4j.Slf4j;
 @SpringBootApplication
 public class BscScanGeckkoApplication {
 
-	public static void main(String[] args) {
-		try {
-			log.info("Start " + Utils.convertDateToString("yyyy-MM-dd HH:mm:ss", new Date()) + " ---->");
+    public static void main(String[] args) {
+        try {
+            log.info("Start " + Utils.convertDateToString("yyyy-MM-dd HH:mm:ss", new Date()) + " ---->");
 
-			ApplicationContext applicationContext = SpringApplication.run(BscScanGeckkoApplication.class, args);
-			bsc_scan_geckko.service.CoinGeckoService gecko_service = applicationContext.getBean(CoinGeckoService.class);
+            ApplicationContext applicationContext = SpringApplication.run(BscScanGeckkoApplication.class, args);
+            bsc_scan_geckko.service.CoinGeckoService gecko_service = applicationContext.getBean(CoinGeckoService.class);
 
-			List<bsc_scan_geckko.entity.CandidateCoin> list = gecko_service.getList();
-			if (CollectionUtils.isEmpty(list)) {
-				gecko_service.initCandidateCoin();
-			}
+            List<bsc_scan_geckko.entity.CandidateCoin> list = gecko_service.getList();
+            if (CollectionUtils.isEmpty(list)) {
+                gecko_service.initCandidateCoin();
+            }
 
-			int size = list.size();
-			int idx = 0;
-			while (idx < size) {
-				CandidateCoin coin = list.get(idx);
+            int size = list.size();
+            int idx = 0;
+            while (idx < size) {
+                CandidateCoin coin = list.get(idx);
 
-				try {
-					gecko_service.loadData(coin.getGeckoid());
-				} catch (Exception e) {
-					log.error("dkd error LoadData:" + e.getMessage());
-					wait(600000);
-				}
+                try {
+                    gecko_service.loadData(coin.getGeckoid());
+                } catch (Exception e) {
+                    log.error("dkd error LoadData:" + e.getMessage());
+                    wait(600000);
+                }
 
-				wait(2000);// 2000ms=30 request/minus
+                wait(2000);// 2000ms=30 request/minus
 
-				log.info("idx:" + String.valueOf(idx) + "/" + size + "; id:" + coin.getGeckoid() + "; Symbol:"
-						+ coin.getSymbol());
+                log.info("idx:" + String.valueOf(idx) + "/" + size + "; id:" + coin.getGeckoid() + "; Symbol:"
+                        + coin.getSymbol());
 
-				if (Objects.equals(idx, size - 1)) {
-					log.info("reload: " + Utils.convertDateToString("yyyy-MM-dd HH:mm:ss", new Date()));
-					idx = 0;
-					list.clear();
-					list = gecko_service.getList();
-					size = list.size();
-				} else {
-					idx += 1;
-				}
-			}
+                if (java.util.Objects.equals(idx, size - 1)) {
+                    log.info("reload: " + Utils.convertDateToString("yyyy-MM-dd HH:mm:ss", new Date()));
+                    idx = 0;
+                    list.clear();
+                    list = gecko_service.getList();
+                    size = list.size();
+                } else {
+                    idx += 1;
+                }
+            }
 
-			log.info("End " + Utils.convertDateToString("yyyy-MM-dd HH:mm:ss", new Date()) + " <----");
-		} catch (Exception e) {
-			log.error(e.getMessage());
-		}
+            log.info("End " + Utils.convertDateToString("yyyy-MM-dd HH:mm:ss", new Date()) + " <----");
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
 
-	}
+    }
 
-	public static void wait(int ms) {
-		try {
-			java.lang.Thread.sleep(ms);
-		} catch (InterruptedException ex) {
-			java.lang.Thread.currentThread().interrupt();
-		}
-	}
+    public static void wait(int ms) {
+        try {
+            java.lang.Thread.sleep(ms);
+        } catch (InterruptedException ex) {
+            java.lang.Thread.currentThread().interrupt();
+        }
+    }
 
 }
