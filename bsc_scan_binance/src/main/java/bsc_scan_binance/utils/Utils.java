7c7988b904e8a44814485c491a393a2318031963
DuyDK <khanhduyapt@gmail.com>
2023-01-27 13:22:20 +0700

    u

--- bsc_scan_binance/src/main/java/bsc_scan_binance/utils/Utils.java
+++ bsc_scan_binance/src/main/java/bsc_scan_binance/utils/Utils.java
@@ -2106,26 +2106,26 @@ public class Utils {
         BigDecimal ma_X_c = calcMA(list, maSlowIndex, 1);
         BigDecimal close1 = calcMA(list, 3, 1); // list.get(1).getPrice_open_candle();
         BigDecimal close2 = calcMA(list, 3, 2); // list.get(2).getPrice_open_candle();
-        boolean m15IsAboveMa50 = Utils.isAboveMALine(list, 50, 1);
 
-        if (!m15IsAboveMa50 && (close1.compareTo(close2) > 0) && (close1.compareTo(ma_X_c) > 0)
-                && (ma_X_c.compareTo(close2) > 0)) {
-            return TREND_LONG;
-        }
+        if (maSlowIndex < 50) {
+            boolean m15IsAboveMa50 = Utils.isAboveMALine(list, 50, 1);
 
-        if (m15IsAboveMa50 && (close1.compareTo(close2) < 0) && (close1.compareTo(ma_X_c) < 0)
-                && (ma_X_c.compareTo(close2) < 0)) {
-            return TREND_SHORT;
-        }
-
-        if (list.get(0).getId().contains("_1m_")) {
-            close2 = calcMA(list, 3, 3);
             if (!m15IsAboveMa50 && (close1.compareTo(close2) > 0) && (close1.compareTo(ma_X_c) > 0)
                     && (ma_X_c.compareTo(close2) > 0)) {
                 return TREND_LONG;
             }
 
             if (m15IsAboveMa50 && (close1.compareTo(close2) < 0) && (close1.compareTo(ma_X_c) < 0)
+                    && (ma_X_c.compareTo(close2) < 0)) {
+                return TREND_SHORT;
+            }
+        } else {
+            if ((close1.compareTo(close2) > 0) && (close1.compareTo(ma_X_c) > 0)
+                    && (ma_X_c.compareTo(close2) > 0)) {
+                return TREND_LONG;
+            }
+
+            if ((close1.compareTo(close2) < 0) && (close1.compareTo(ma_X_c) < 0)
                     && (ma_X_c.compareTo(close2) < 0)) {
                 return TREND_SHORT;
             }
