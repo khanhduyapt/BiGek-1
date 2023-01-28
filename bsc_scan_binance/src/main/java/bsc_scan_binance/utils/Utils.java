73a6e22caad36044a59af996adf5e7ca00ef6fd6
khanhduyapt <67938470+khanhduyapt@users.noreply.github.com>
2023-01-28 16:49:40 +0700

    u

--- bsc_scan_binance/src/main/java/bsc_scan_binance/utils/Utils.java
+++ bsc_scan_binance/src/main/java/bsc_scan_binance/utils/Utils.java
@@ -2228,7 +2228,7 @@ public class Utils {
             }
         }
 
-        List<BigDecimal> open_close = getLowHeightCandle(list.subList(1, 2));
+        List<BigDecimal> open_close = getLowHeightCandle(list.subList(1, 3));
         BigDecimal low = open_close.get(0);
         BigDecimal hig = open_close.get(1);
         if ((ma10_1.compareTo(ma20_1) > 0) && (hig.compareTo(ma10_1) > 0) && (ma10_1.compareTo(low) > 0)
@@ -2297,7 +2297,7 @@ public class Utils {
             }
         }
 
-        List<BigDecimal> open_close = getLowHeightCandle(list.subList(1, 2));
+        List<BigDecimal> open_close = getLowHeightCandle(list.subList(1, 3));
         BigDecimal low = open_close.get(0);
         BigDecimal hig = open_close.get(1);
         if ((ma20_1.compareTo(ma10_1) > 0) && (hig.compareTo(ma10_1) > 0) && (ma10_1.compareTo(low) > 0)
